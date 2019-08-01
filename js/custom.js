68f606c0c0cefe6b4928fa6d6e2d7c610c4d3ec6
kimia naderi <kimiakhojasteh93@gmail.com>
2019-08-01 23:39:34 +0430

    editing

--- js/custom.js
+++ js/custom.js
@@ -62,6 +62,7 @@
 		  $("#owl-demo").owlCarousel({
 			  autoPlay: 3000, //Set AutoPlay to 3 seconds
 			  items : 3,
+			  rtl:true,
 			  itemsDesktop : [1199,3],
 			  itemsDesktopSmall : [979,2]
 		 
